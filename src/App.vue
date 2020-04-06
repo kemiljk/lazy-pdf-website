ea0119095b6bead59a9dd1005fa4b2043e97b81f
Karl Koch <hi@kejk.co>
2020-04-06 20:11:11 +0100

    fix: missing space

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar 
-      app 
-      color="#2C1E33"
-    >
-      <v-img 
-        src="./assets/Lazy-PDF-icon.png"
-        max-height="40"
-        max-width="40"
-        contain
-      />
+    <v-app-bar app color="#2C1E33">
+      <v-img src="./assets/Lazy-PDF-icon.png" max-height="40" max-width="40" contain />
       <v-spacer />
       <v-btn
         v-for="link in links"
@@ -18,33 +10,26 @@
         text
         :to="link.url"
       >
-        <v-text class="font-weight-bold">
-          {{ link.label }}
-        </v-text>
+        <v-text class="font-weight-bold">{{ link.label }}</v-text>
       </v-btn>
       <v-btn
         class="my-2 mx-1"
         text
         href="https://res.cloudinary.com/kejk/raw/upload/v1585911201/Lazy_PDF.zip"
       >
-        <v-text class="font-weight-bold">
-          Download
-        </v-text>
-      </v-btn> 
+        <v-text class="font-weight-bold">Download</v-text>
+      </v-btn>
     </v-app-bar>
     <v-content>
       <v-responsive width="900px">
         <router-view />
       </v-responsive>
     </v-content>
-    <v-footer 
-      app
-      color="#2C1E33"
-    >
+    <v-footer app color="#2C1E33">
       <v-col cols="12">
         <strong>
           Made with
-          <v-icon>mdi-vuejs</v-icon> in London.
+          <v-icon>mdi-vuejs</v-icon>&nbsp;in London.
         </strong>
       </v-col>
     </v-footer>
